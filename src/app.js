af3a5d9add5e1bd6bac74744833c6cd26be034da
tonyarice <72225215+tonyarice@users.noreply.github.com>
2021-07-11 00:23:44 -0400

    Added weather icons from the API

--- src/app.js
+++ src/app.js
@@ -23,23 +23,30 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
+  //   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let currentConditionsElement = document.querySelector("#current-conditions");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   currentConditionsElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  //   iconElement.setAttribute("alt, response.data.weather[0].main);
 }
 
 let apiKey = "fdcd56ea6e53c9d3c7330d2c7397cff6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Richmond&appid=${apiKey}&units=imperial`;
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 //console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
