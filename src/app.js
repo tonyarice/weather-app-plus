d524d29215ca279d64db533c46b835dd6ba903da
tonyarice <72225215+tonyarice@users.noreply.github.com>
2021-07-13 10:02:33 -0400

    Implemented units conversion

--- src/app.js
+++ src/app.js
@@ -31,6 +31,9 @@ function displayTemperature(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  fahrenheitTemperature = response.data.main.temp;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   currentConditionsElement.innerHTML = response.data.weather[0].description;
@@ -48,6 +51,7 @@ function search(city) {
   let apiKey = "fdcd56ea6e53c9d3c7330d2c7397cff6";
   //   let city = "Richmond";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
   //console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -58,7 +62,37 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Richmond");
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  //remove the active class to the fahrenheit link
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+  //   alert(celsiusTemperature);
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("Richmond");
