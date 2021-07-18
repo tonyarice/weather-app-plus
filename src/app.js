99c60483a709f97735f755d2d7bba3ac76b4cac2
tonyarice <72225215+tonyarice@users.noreply.github.com>
2021-07-17 20:21:09 -0400

    added daily forecast (still with fake data for now)

--- src/app.js
+++ src/app.js
@@ -22,6 +22,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="60" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-high">82°</span>
+          <span class="weather-forecast-temperature-low"> 75°</span>
+        </div>
+      
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + "</div>";
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "fdcd56ea6e53c9d3c7330d2c7397cff6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   //   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -44,7 +79,9 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  //   iconElement.setAttribute("alt, response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
